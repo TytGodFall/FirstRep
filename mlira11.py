6393868ed40c63db2fb0733d3589b5035bc645b3
TytGodFall <ctcuser112@ip-172-31-46-179.us-east-2.compute.internal>
2023-11-10 02:45:29 +0000

    lab 11 python program

---
+++ mlira11.py
@@ -0,0 +1,16 @@
+#Matthew Lira
+#CSC-321
+#Lab 11-->11/9/23
+
+
+import math
+
+print("Squared Calculator\n")
+
+input_Str = input("Please enter a number: ")
+
+number = float(input_Str)
+
+result = math.pow(number,2)
+
+print("Based on the Given Number, We got a squared answer of: ",result) 
